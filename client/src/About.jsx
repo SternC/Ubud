7448c87dc2aff699d4581cf55c4b7b2cd9a87b38
Your Name <richardsternchandra@gmail.com>
2025-09-18 22:56:24 +0700

    React Router DOM

---
+++ client/src/About.jsx
@@ -0,0 +1,11 @@
+import react, { useState, useEffect } from "react";
+
+function About(){
+    return (
+        <div>
+            <p>On Progress</p>
+        </div>
+    )
+}
+
+export default About;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import './index.css'
 import App from './App.jsx'
+import About from './About.jsx'
+
+const routerPath = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routerPath} />
   </StrictMode>,
 )
