f9ddbb8022d49dc708e1d97d8dace49ecc95cfa7
michi <michelleoliviah16@gmail.com>
2025-09-22 01:57:23 +0700

    card

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 import Hero from "./hero";
-
+import Card from "./card";
 function App() {
   return (
     <>
-      <Hero />
+      <Card />
     </>
   );
 }
